f64da3629c354c654397838dc32cda5e8d268d9c
Алексей Иванцив <alivanciv@gmail.com>
2024-03-03 15:08:36 +1100

    Sprint15. Fodgram. #2

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -5,9 +5,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['buyfood.ddns.net', 'localhost']
+ALLOWED_HOSTS = ['51.250.23.117', '127.0.0.1', 'localhost', 'buyfood.ddns.net']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
